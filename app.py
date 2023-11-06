9f9a9dd07afbcba6d7b537fecf61af9d4d822242
Nishant Raghuwanshi <110217583+Nishant2018@users.noreply.github.com>
2023-11-06 12:11:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -55,4 +55,5 @@ def predict():
         return render_template('index.html', prediction_text='Predicted class: {}'.format(prediction_class))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port=8080)
+    #app.run(debug=True)
